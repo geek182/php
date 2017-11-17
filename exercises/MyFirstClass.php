e67a1eec656c43ad3423fef1d81089f8d62cc592
Leandro Azevedo <geek182@gmail.com>
2017-11-17 15:52:30 -0200

    function with construct

--- exercises/MyFirstClass.php
+++ exercises/MyFirstClass.php
@@ -1,7 +1,13 @@
-<?php
+    <?php
 class MyClass {
     /* This is a comment */
     var $myvar ;
+    var $myvar_with_construct ;
+    
+    /* Construct function */
+    function __construct($arg1){
+        $this->myvar_with_construct = $arg1 ;  
+    }
 
     function SetVar($var){
         $this->myvar = $var ;
@@ -9,10 +15,13 @@ class MyClass {
     function GetVar(){
      echo $this->myvar ." <br/>" ;
     }
+    function GetVarWithConstruct(){
+        echo $this->myvar_with_construct;
+    }
 }
 /* instance object from Class */
-$Myobj1 = new MyClass ;
-$Myobj2 = new MyClass ;
+$Myobj1 = new MyClass(10) ;
+$Myobj2 = new MyClass(11) ;
 
 /* Set value to a function */
 $Myobj1->SetVar(10);
@@ -21,4 +30,7 @@ $Myobj2->SetVar(300);
 /* Get value from a function */
 $Myobj1->GetVar();
 $Myobj2->GetVar();
+$Myobj1->GetVarWithConstruct();
+$Myobj2->GetVarWithConstruct();
+
 ?>
